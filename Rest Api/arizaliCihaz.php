c84909a17e4c84a915f7b47dd2c4b58d4a0bade1
Mustafa Ersoy <mustafaersoy@users.noreply.github.com>
2018-11-22 21:56:41 +0300

    Rest Api Added

---
+++ Rest Api/arizaliCihaz.php
@@ -0,0 +1,54 @@
+<?php
+include "db.php";
+include "function.php";
+$islem = isset($_GET["islem"]) ? addslashes(trim($_GET["islem"])) : null;
+$jsonArray = array(); // array değişkenimiz bunu en alta json objesine çevireceğiz.
+$jsonArray["hata"] = FALSE; // Başlangıçta hata yok olarak kabul edelim.
+
+$_code = 200; // HTTP Ok olarak durumu kabul edelim.
+$_method = $_SERVER["REQUEST_METHOD"]; // client tarafından bize gelen method
+// aldığımız işlem değişkenine göre işlemler yapalım.
+  if($_method  == "POST") {
+  }
+  else if($_method == "PUT") {
+  }
+  else if($_method == "DELETE") {
+  }
+  else if($_method == "GET") {
+    if(isset($_GET["tesisId"])) {
+
+       $tesisId= ($_GET["tesisId"]);
+       //if(tarih: yıl ise aylik uretim çalıştır)
+      //else (tarih: yıl-ay-gun ise saatlik uretim çalıştır)
+       $bilgiler = $db->query("SELECT * FROM arizalicihazlar where tesisId like tesisId='$tesisId'", PDO::FETCH_ASSOC);
+       if($bilgiler->rowCount()>0) {
+         $_code = 200;
+         $i=1;
+         foreach( $bilgiler as $row ){
+            $jsonArray["bilgi".$i] = $row;
+            $i=$i+1;
+         }
+       }
+       else {
+        $_code = 400;
+        $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+        $jsonArray["hataMesaj"] = "Kayıt bulunamadı"; // Hatanın neden kaynaklı olduğu belirtilsin.
+       }
+    }
+    else{
+       $_code = 400;
+       $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+       $jsonArray["hataMesaj"] = "Lütfen tesisId değişkeni gönderin"; // Hatanın neden kaynaklı olduğu belirtilsin.
+    }
+
+  }
+  else {
+    // hatalı bir parametre girilmesi durumunda burası çalışacak.
+    $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+    $jsonArray["hataMesaj"] = "Girilen İşlem Bulunmuyor."; // Hatanın neden kaynaklı olduğu belirtilsin.
+  }
+
+SetHeader($_code);
+$jsonArray[$_code] = HttpStatus($_code);
+echo json_encode($jsonArray);
+?>
---
+++ Rest Api/giris.php
@@ -0,0 +1,45 @@
+﻿<?php
+include "db.php";
+include "function.php";
+$islem = isset($_GET["islem"]) ? addslashes(trim($_GET["islem"])) : null;
+$jsonArray = array(); // array değişkenimiz bunu en alta json objesine çevireceğiz.
+$jsonArray["hata"] = FALSE; // Başlangıçta hata yok olarak kabul edelim.
+
+$_code = 200; // HTTP Ok olarak durumu kabul edelim.
+$_method = $_SERVER["REQUEST_METHOD"]; // client tarafından bize gelen method
+// aldığımız işlem değişkenine göre işlemler yapalım.
+if($_method  == "POST") {
+       $jsonArray["giris"]=0;
+     	// verilerimizi post yöntemi ile alalım.
+        $kullaniciAdi1 = addslashes($_POST["kullaniciAdi1"]);
+        $sifre1 = addslashes($_POST["sifre1"]);
+
+        $uye = $db->query("SELECT * from kullanici WHERE kAdi='$kullaniciAdi1' && kSifre='$sifre1'")->fetch(PDO::FETCH_ASSOC);
+
+        
+        if($uye) {
+        	  $_code = 200;
+            $jsonArray["hata"] = FALSE ;  // bir hata yok.
+            $jsonArray["giris"]=1;
+            $jsonArray["tesisId"]= $uye['tesisId'];
+          
+        }else {
+          $_code = 400;
+           $jsonArray["hata"] =TRUE ;  
+           $jsonArray["giris"]=0;
+            $jsonArray["hataMesaj"] = "Kullanıcı Adı veya Şifre yanlış.";
+         }
+  }else if($_method == "PUT") {
+  }else if($_method == "DELETE") {
+  }else if($_method == "GET") {
+  }
+  else {
+        // hatalı bir parametre girilmesi durumunda burası çalışacak.
+        $jsonArray["hata"] = TRUE; // bir hata olduğu bildirilsin.
+        $jsonArray["hataMesaj"] = "Girilen İşlem Bulunmuyor."; // Hatanın neden kaynaklı olduğu belirtilsin.
+  }
+
+SetHeader($_code);
+$jsonArray[$_code] = HttpStatus($_code);
+echo json_encode($jsonArray);
+?>
